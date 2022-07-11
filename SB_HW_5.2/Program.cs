9cebe2877cf4c2c4cc9ae39acf0c584b2a62a2dc
User <karaev.e.a@yandex.ru>
2022-07-11 21:41:50 +0600

    Добавьте файлы проекта.

---
+++ SB_HW_5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿static string GetSentence(string userSentence)
+{
+    string[] array = userSentence.Split(' ');
+    string ReverseUserSentence = ReverseWord(array);
+    return ReverseUserSentence;
+}
+static string ReverseWord(string[] userSentence) // 
+{
+    Array.Reverse(userSentence);
+    string str = string.Join(" ", userSentence);
+    return str;
+}
+
+
+Console.WriteLine("Введите предложение: ");
+
+string text = Console.ReadLine();
+
+text = GetSentence(text);
+
+
+Console.WriteLine(text);
+Console.ReadLine();
---
+++ SB_HW_5/Program.cs
@@ -0,0 +1,21 @@
+﻿static void GetSentence(string userSentence)
+{
+    string[] array = { userSentence };
+
+}
+static string GetSpliteUserSentences(string userSentence)
+{
+    string[] array = userSentence.Split(' ');
+    foreach (var arr in array)
+    {
+        Console.WriteLine(arr);
+    }
+
+    return array[0];
+}
+
+
+Console.WriteLine("Введите предложение");
+string userSentence = Console.ReadLine();
+
+GetSpliteUserSentences(userSentence);
